6e0aeef52a38fc7482a2566cffd6747230fb5ee7
ryan-chua <94115040+ryan-chua@users.noreply.github.com>
2023-11-15 21:58:22 +0800

    Update monitor.c

--- 2004project/monitor.c
+++ 2004project/monitor.c
@@ -1,25 +1,3 @@
-// PicoWi DHCP example, see https://iosoft.blog/picowi
-//
-// Copyright (c) 2022, Jeremy P Bentham
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -40,8 +18,8 @@
 #include "lib/picowi_scan.h"
 
 // The hard-coded password is for test purposes only!!!
-#define SSID   "HuaweiReno"
-#define PASSWD   "016f1fd3305a"
+#define SSID   "test"
+#define PASSWD   "test"
 #define EVENT_POLL_USEC     100000
 #define PING_RESP_USEC      200000
 #define PING_DATA_SIZE      32
